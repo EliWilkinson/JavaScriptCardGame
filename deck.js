18011b4c7f6dc8ff5efe71bd2677eeb4b03da79b
EliWilkinson <eli.wilkinson9@gmail.com>
2022-03-14 22:25:18 -0500

    deck of cards setup

---
+++ deck.js
@@ -0,0 +1,39 @@
+//good for making games other than war
+
+const SUITS = ["♥", "♦", "♠", "♣"]
+const VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+export default class Deck {
+    constructor(cards= freshDeck()) {  // =freshDeck() gives us a limit on cards
+        this.cards = cards
+    }
+
+get numberOFCards() {
+    return this.cards.length
+}
+
+    shuffle() {
+        //this.cards.sort((a,b) => Math.random()- .5).............this actually isn't random
+        for(let i = this.numberOFCards-1; i>0; i--) {
+            const newIndex = Math.floor(Math.random() * (i+1))
+            const oldValue = this.cards[newIndex]
+            this.cards[newIndex] = this.cards[i]
+            this.cards[i] = oldValue
+        }
+    }
+}
+
+class Card {
+    constructor(suit, value) {
+        this.suit = suit
+        this.value = value
+    }
+}
+
+function freshDeck() {
+    return SUITS.flatMap(suit => {
+        return VALUES.map(value=> {
+            return new Card(suit, value) 
+        })
+    })
+}
