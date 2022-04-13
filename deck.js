7e3b1131667007618d66cadf58a0e1fe0e8faa2e
EliWilkinson <eli.wilkinson9@gmail.com>
2022-04-13 12:28:33 -0500

    up to 15:46 in video

--- deck.js
+++ deck.js
@@ -11,9 +11,9 @@ export default class Deck {
 get numberOFCards() {
     return this.cards.length
 }
-
+ 
     shuffle() {
-        //this.cards.sort((a,b) => Math.random()- .5).............this actually isn't random
+        //this.cards.sort((a,b) => Math.random()- .5)...this isn't random
         for(let i = this.numberOFCards-1; i>0; i--) {
             const newIndex = Math.floor(Math.random() * (i+1))
             const oldValue = this.cards[newIndex]
@@ -31,6 +31,8 @@ class Card {
 }
 
 function freshDeck() {
+    //.flatmap combines the arrays of suits into one of 52
+    //.map would return 4 arrays of 13 instead, harder to work with here
     return SUITS.flatMap(suit => {
         return VALUES.map(value=> {
             return new Card(suit, value) 
